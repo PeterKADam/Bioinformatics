82b0f7a5db1e724f5d98d8faec8d923c29db309f
PeterKADam <89773385+PeterKADam@users.noreply.github.com>
2021-10-08 13:09:13 +0200

    alignmentproject start

--- Alignmentproject/alignmentproject.py
+++ Alignmentproject/alignmentproject.py
@@ -2,6 +2,9 @@
 # Write your functions definitions below.
 # Do not write any other code here.
 #############################################################
+from inspect import trace
+
+
 MATCH_SCORES = {
     "A": {"A": 2, "T": 0, "G": 0, "C": 0},
     "T": {"A": 0, "T": 2, "G": 0, "C": 0},
@@ -46,9 +49,97 @@ def prepare_matrix(len_seq1: int, len_seq2: int, gap_score: int):
     return matrix
 
 
-def fill_matrix():
+def fill_matrix(seq1, seq2, dictdict, gap_score):
 
-    return
+    matrix = prepare_matrix(len(seq1) + 1, len(seq2) + 1, gap_score)
+
+    for y in range(1, len(seq1) + 1):
+        for x in range(1, len(seq2) + 1):
+            matrix[y][x] = max(
+                matrix[y][x - 1] + gap_score,
+                matrix[y - 1][x] + gap_score,
+                matrix[y - 1][x - 1] + dictdict[seq1[y - 1]][seq2[x - 1]],
+            )
+
+    return matrix
+
+
+def get_traceback_arrow(matrix, row, col, match_score, gap_score):
+    # yellow
+    score_diagonal = matrix[row - 1][col - 1]
+    score_left = matrix[row][col - 1]
+    score_up = matrix[row - 1][col]
+
+    # green
+    score_current = matrix[row][col]
+
+    if score_current == score_diagonal + match_score:
+        return "diagonal"
+    elif score_current == score_left + gap_score:
+        return "left"
+    elif score_current == score_up + gap_score:
+        return "up"
+
+
+def trace_back(seq1, seq2, matrix, score_matrix, gap_score):
+
+    # Strings to store the growing alignment strings:
+    aligned1 = ""
+    aligned2 = ""
+    # continues...
+    # The row and col index of the bottom right cell:
+    row = len(seq1)
+    col = len(seq2)
+    # Keep stepping backwards through the matrix untill
+    # we get to the top row or the left col:
+    while row > 0 and col > 0:
+        # The two bases we available to match:
+        base1 = seq1[row - 1]
+        base2 = seq2[col - 1]
+        # The score for mathing those two bases:
+        match_score = score_matrix[base1][base2]
+        # Find out which cell the score in the current cell was derived from:
+        traceback_arrow = get_traceback_arrow(matrix, row, col, match_score, gap_score)
+        if traceback_arrow == "diagonal":
+            # last column of the sub alignment is base1 over base2:
+            aligned1 = base1 + aligned1
+            aligned2 = base2 + aligned2
+            # next cell is the diagonal cell:
+            row -= 1
+            col -= 1
+        elif traceback_arrow == "up":
+            # last column in the sub alignment is base1 over a gap:
+            aligned1 = base1 + aligned1
+            aligned2 = "-" + aligned2
+            # next cell is the cell above:
+            row -= 1
+        elif traceback_arrow == "left":
+            # last column in the sub alignment is a gap over base2:
+            aligned1 = "-" + aligned1
+            aligned2 = base2 + aligned2
+            # next cell is the cell to the left:
+            col -= 1
+            # continues...
+            # If row is not zero, step along the top row to the top left cell:
+        while row > 0:
+            base1 = seq1[row - 1]
+            aligned1 = base1 + aligned1
+            aligned2 = "-" + aligned2
+            row -= 1
+            # If col is not zero, step upwards in the left col to the top left cell:
+        while col > 0:
+            base2 = seq2[col - 1]
+            aligned1 = "-" + aligned1
+            aligned2 = base2 + aligned2
+            col -= 1
+        return [aligned1, aligned2]
+
+
+def align(seq1, seq2, score_matrix, gap_score):
+
+    matrix = fill_matrix(seq1, seq2, score_matrix, gap_score)
+
+    return trace_back(seq1, seq2, matrix, score_matrix, gap_score)
 
 
 #############################################################
@@ -59,5 +150,6 @@ def fill_matrix():
 
 # any other code ...
 
-print(prepare_matrix(3, 4, -2))
 
+# print(fill_matrix("AT", "GAT", MATCH_SCORES, -2))
+print(align("ATAT", "GATGAT", MATCH_SCORES, -2))
